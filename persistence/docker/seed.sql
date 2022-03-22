ef5a1599519075092b404694c6b38077f9f04ac0
Denis Kabishev <kabishev@gmail.com>
2022-03-22 18:19:21 +0100

    Add database initialization

--- persistence/docker/seed.sql
+++ persistence/docker/seed.sql
@@ -2,6 +2,11 @@ BEGIN;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-
+CREATE TABLE IF NOT EXISTS article (
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  title text NOT NULL,
+  text text NOT NULL,
+  date timestamp NOT NULL
+);
 
 COMMIT;
